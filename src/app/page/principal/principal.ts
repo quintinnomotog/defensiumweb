1399ad295bcbe3a4d0e73900618e0babc4add254
José Quintinno <josequintino@hotmail.com.br>
2025-07-22 20:22:37 -0300

    feature: Configurar Reactive Forms para cadastro de Credenciais

--- src/app/page/principal/principal.ts
+++ src/app/page/principal/principal.ts
@@ -1,16 +1,27 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-principal',
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule
   ],
   templateUrl: './principal.html',
   styleUrl: './principal.scss',
 })
-export class Principal {
+export class Principal implements OnInit {
+
+  public credencialFormGroup!: FormGroup;
+
   public isApresentarModal: boolean = false;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.configurarFormulario();
+  }
 
   public apresentarModal() {
     this.isApresentarModal = true;
@@ -18,6 +29,21 @@ export class Principal {
 
   public fecharModal() {
     this.isApresentarModal = false;
+  }
+
+  private configurarFormulario() {
+    this.credencialFormGroup = new FormGroup({
+      pessoaCodigo: new FormControl("", [Validators.required]),
+      descricao: new FormControl("", [Validators.required, Validators.minLength(3), Validators.maxLength(100)]),
+      identificador: new FormControl("", [Validators.required, Validators.minLength(3), Validators.maxLength(60)]),
+      senha: new FormControl("", [Validators.required, Validators.minLength(3), Validators.maxLength(50)]),
+      link: new FormControl("", [Validators.minLength(3), Validators.maxLength(255)]),
+      observacao: new FormControl("", [Validators.minLength(3), Validators.maxLength(255)]),
+    });
+  }
+
+  public cadastrarCredencial() {
+    console.table(this.credencialFormGroup.value);
   }
 
 }
