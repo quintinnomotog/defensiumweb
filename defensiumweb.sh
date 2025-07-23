96e020afdd789676ffd1139f7bf528970ad30b83
José Quintinno <josequintino@hotmail.com.br>
2025-07-23 06:53:43 -0300

    build: Gerar versão de implantação

--- defensiumweb.sh
+++ defensiumweb.sh
@@ -3,3 +3,7 @@ ng serve &
 wait 8
 
 chromium --app=http://127.0.0.1:4200
+
+# npm install -g angular-cli-ghpages
+# ng build --base-href defensiumweb --configuration production
+# ng build --output-path docs
